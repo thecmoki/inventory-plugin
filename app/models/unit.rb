e5e0965c0172803044ae818a0b3e81f96200cfe3
Fitim Gashi <fitim@asdasdasd.com>
2016-02-12 14:15:28 +0100

    Validimi i Room dhe Unit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,10 @@
 class Unit < ActiveRecord::Base
 	has_many(:stocks)
+
+	attr_accessible :name, :comment
+
+	validates :name, :presence => true
+	validates :comment, :length => { :maximum => 100}
+
+		
 end

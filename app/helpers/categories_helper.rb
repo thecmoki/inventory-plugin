74158e0af1e5621fdf8b57f7e095f5db4d698504
Valon Hajredini <hajredini.valon@gmail.com>
2016-03-07 16:59:49 +0100

    rregullimi i categoryes

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,16 @@
 module CategoriesHelper
+	def name(lan = "en")
+		if(lan == "en")
+			"Name"
+		elsif(lan == "al")
+			"Emri"
+		end
+	end
+	def comment(lan = "en")
+		if(lan == "en")
+			"Comment"
+		elsif(lan == "al")
+			"Komenti"
+		end
+	end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ActiveRecord::Base
 	has_many(:units)
-
 	validates :name, :presence => true
 	validates :comment, :length => { :maximum => 100}
 end

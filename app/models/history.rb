206ce8e2cf7f8b2665da090639b9ff8e4a7f86fd
Ilir Gojani <iliri_gojani@hotmail.com>
2016-03-08 22:57:05 +0100

    Rregullimi qe me e llogarit ditet se sa eshte perdorur nje produkt

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,20 @@
 class History < ActiveRecord::Base
   belongs_to(:user)
   belongs_to(:inventory)
+  def self.ransackable_attributes(auth_object = nil)
+		if(User.current.admin == true)
+			super - ['id', 'created_at', 'updated_at']
+		else
+			super - ['id', 'created_at', 'updated_at', 'user_name', 'room_name', 'user_login']
+		end
+	end
+
+	def toDays
+		years = updated_at.year - activation_date.year
+		yd = years * 365.25
+		months = updated_at.month - activation_date.month
+		md = (months / 12.0) * 365.25
+		days = updated_at.day - activation_date.day
+		(yd + md + days).to_i
+	end
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -9,11 +9,20 @@ class Inventory < ActiveRecord::Base
 	validates(:product_name, :presence => true)
 	validates(:product_id, :presence => true)
 	validates(:serial_number, :presence => true, :uniqueness => true)
+	validates(:activation_date, :presence => true)
 	def self.ransackable_attributes(auth_object = nil)
 		if(User.current.admin == true)
 			super - ['id', 'created_at', 'updated_at']
 		else
 			super - ['id', 'created_at', 'updated_at', 'user_name', 'room_name', 'user_login']
 		end
+	end
+	def toDays
+		years = Time.now.year - activation_date.year
+		yd = years * 365.25
+		months = Time.now.month - activation_date.month
+		md = (months / 12.0) * 365.25
+		days = Time.now.day - activation_date.day
+		(yd + md + days).to_i
 	end
 end
